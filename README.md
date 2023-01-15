ab77371f74ff102170c15e3696f68aab1d0dac1e
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-01-15 12:41:57 +0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# the-t3-boilerplate
---
+++ apps/nextjs/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@acme/ui";
+
+const Home = () => {
+  return (
+    <div>
+      <Button variant="destructive">Hello World</Button>
+    </div>
+  );
+};
+
+export default Home;
